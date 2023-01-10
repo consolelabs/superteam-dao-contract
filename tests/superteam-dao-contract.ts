586d85a09646988b4cca3f9ebe0e64227e23bc6a
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-01-10 17:40:30 +0700

    feat: add proof of payment status

--- tests/superteam-dao-contract.ts
+++ tests/superteam-dao-contract.ts
@@ -33,9 +33,10 @@ describe("superteam-dao-contract", () => {
     let identifierAccount2, approverProposal, approverProposal1, approverProposal2 : anchor.web3.PublicKey;
     let bump: number;
     let mintA;
+    const delay = ms => new Promise(res => setTimeout(res, ms));
 
     before("Boilerplates", async () => {
-        const delay = ms => new Promise(res => setTimeout(res, ms));
+
         await delay(1000*5);
         await airdrop(provider, sender.publicKey, airdropSolAmount)
         await delay(1000*5);
@@ -67,7 +68,7 @@ describe("superteam-dao-contract", () => {
         [applicantProposal, bump] = await findPDAProposal(sender.publicKey, identifierData.count, program)
 
         await program.methods.createProposal(recipient.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
-            "Orca summer Winner", "abc", mintA, "gamefi", new BN(100*(10**MINT_A_DECIMALS)), true, "")
+            "Orca summer Winner", "abc", mintA, "gamefi", new BN(100*(10**MINT_A_DECIMALS)), true, null)
             .accounts({
                 proposal: applicantProposal,
                 identifier: identifierAccount,
@@ -86,7 +87,7 @@ describe("superteam-dao-contract", () => {
         [applicantProposal1, bump] = await findPDAProposal(sender.publicKey, identifierData.count, program)
 
         await program.methods.createProposal(recipient.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
-            "Orca summer 2nd", "", mintA, "defi", new BN(100*(10**MINT_A_DECIMALS)), true, "")
+            "Orca summer 2nd", "", mintA, "defi", new BN(100*(10**MINT_A_DECIMALS)), true, null)
             .accounts({
                 proposal: applicantProposal1,
                 identifier: identifierAccount,
@@ -102,7 +103,7 @@ describe("superteam-dao-contract", () => {
         [applicantProposal2, bump] = await findPDAProposal(sender.publicKey, identifierData.count, program)
 
         await program.methods.createProposal(recipient.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
-            "Orca summer 3rd", "", mintA, "orca", new BN(100*(10**MINT_A_DECIMALS)), true, "")
+            "Orca summer 3rd", "", mintA, "orca", new BN(100*(10**MINT_A_DECIMALS)), true, null)
             .accounts({
                 proposal: applicantProposal2,
                 identifier: identifierAccount,
@@ -164,6 +165,7 @@ describe("superteam-dao-contract", () => {
     });
 
     it("[Applicant flow] applicant reject proposal", async () => {
+        await delay(1000*2);
         await program.methods.applicantRejectProposal()
             .accounts({
                 proposal: applicantProposal,
@@ -206,8 +208,7 @@ describe("superteam-dao-contract", () => {
         console.log("[applicantProposal ] Create result: ", proposalApproveData);
     });
 
-
-    it("[Applicant flow] close cancel or reject  proposal", async () => {
+    it("[Applicant flow] close reject  proposal", async () => {
 
         const balanceBefore = await provider.connection.getBalance(sender.publicKey);
         console.log("[sender balance before close proposal]: ", balanceBefore);
@@ -229,6 +230,91 @@ describe("superteam-dao-contract", () => {
 
     });
 
+    it("[Approver flow] initialize identifier and proposal", async () => {
+
+        [identifierAccount2] = await findPDAIdentifier(recipient.publicKey, program)
+
+        await program.methods.createIdentifier()
+            .accounts({
+                identifier: identifierAccount2,
+                sender: recipient.publicKey,
+                systemProgram: SystemProgram.programId,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+            })
+            .signers([recipient])
+            .rpc();
+
+        let identifierData2 = await program.account.identifier.fetch(identifierAccount2);
+        console.log("[identifier account] Create result: ", identifierData2);
+
+
+        // Create our Token A Mint
+        mintA = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+
+        [approverProposal, bump] = await findPDAProposal(recipient.publicKey, identifierData2.count, program)
+
+        await program.methods.createProposal(sender.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
+            "Global Winner", "abc", mintA, "defi", new BN(100*(10**MINT_A_DECIMALS)), false, null)
+            .accounts({
+                proposal: approverProposal,
+                identifier: identifierAccount2,
+                sender: recipient.publicKey,
+                systemProgram: SystemProgram.programId,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+            })
+            .signers([recipient])
+            .rpc();
+
+        const dataApproverProposal = await program.account.proposal.fetch(approverProposal);
+        console.log("[proposal account] Create result: ", dataApproverProposal);
+
+        identifierData2 = await program.account.identifier.fetch(identifierAccount2);
+
+        [approverProposal1, bump] = await findPDAProposal(recipient.publicKey, identifierData2.count, program)
+
+        await program.methods.createProposal(sender.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
+            "Global 2nd", "", mintA, "gamefi", new BN(100*(10**MINT_A_DECIMALS)), false, "32YZicRkcGKZ46ZPcW3FQ92nG5LDAgkuL1qYExL5HkF9MsKfGPhwXoCvvtQ1oCJ5yqeZCekjutcCZyeAoFhMRXQV")
+            .accounts({
+                proposal: approverProposal1,
+                identifier: identifierAccount2,
+                sender: recipient.publicKey,
+                systemProgram: SystemProgram.programId,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+            })
+            .signers([recipient])
+            .rpc();
+
+        identifierData2 = await program.account.identifier.fetch(identifierAccount2);
+
+        [approverProposal2, bump] = await findPDAProposal(recipient.publicKey, identifierData2.count, program)
+
+        await program.methods.createProposal(recipient.publicKey, "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
+            "Global 3rd", "", mintA, "payment", new BN(100*(10**MINT_A_DECIMALS)), false, null)
+            .accounts({
+                proposal: approverProposal2,
+                identifier: identifierAccount2,
+                sender: recipient.publicKey,
+                systemProgram: SystemProgram.programId,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+            })
+            .signers([recipient])
+            .rpc();
+    });
+
+    it("[Approver flow] approve proposal and confirm proposal", async () => {
+        await program.methods.approveProposal()
+            .accounts({
+                proposal: approverProposal1,
+                recipient: sender.publicKey,
+                systemProgram: SystemProgram.programId,
+                tokenProgram: TOKEN_PROGRAM_ID,
+            })
+            .signers([sender])
+            .rpc();
+
+        let proposalApproveAndConfirmData= await program.account.proposal.fetch(approverProposal1);
+        console.log("[approver proposal ] Create result: ", proposalApproveAndConfirmData);
+    });
 
     // it("filter proposal by sender ", async () => {
     //     const connection = new Connection(clusterApiUrl('devnet'))
